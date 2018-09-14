21670fb23eee1f0a5053b6974638d8ffd5269f48
Roger Winright <venpresath@live.com>
2018-09-14 00:47:03 -0400

    updated links

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
-const newLocal = $.get("https://www.reddit.com/r/comics/.json", (data) => {
-    
-    for (i = 0; i < 18; i++) {
-        let title = data.data.children[i].data.title;
-        let image = data.data.children[i].data.thumbnail;
-        let link = `https://reddit.com${data.data.children[i].data.permalink}`;
-        let mainImage = data.data.children[i].data.preview.images[0].source.url;
-        console.log(mainImage)
-        console.log(title);
-        console.log(image);
-        console.log(link);
-        function np() {
-            let newPost = $(`<div class="post">
-                                <div class="articleTitle">
-                                    <h1>${title}</h1>
-                                </div>
-                                <div class="articleLink">
-                                    <a href="${link}">${link}</a>
-                                </div>
-                                <div class="articleImage">
-                                    <img src="${mainImage}">
-                                </div>
-                            </div>`);
-            $(".posts").append(newPost);
+
+    const newLocal = $.get(`https://www.reddit.com/r/all/.json`, (data) => {
+
+        for (i = 0; i < 18; i++) {
+            let title = data.data.children[i].data.title;
+            let image = data.data.children[i].data.thumbnail;
+            let link = `https://reddit.com${data.data.children[i].data.permalink}`;
+            let mainImage = data.data.children[i].data.preview.images[0].source.url;
+            function np() {
+                let newPost = $(`<div class="post">
+                            <div class="articleTitle">
+                                <h1>${title}</h1>
+                            </div>
+                            <div class="articleLink">
+                                <a href="${link}">Click here for article</a>
+                            </div>
+                            <div class="articleImage">
+                                <img src="${mainImage}">
+                            </div>
+                        </div>`);
+                $(".posts").append(newPost);
+            }
+            np();
+            
         }
-        np();
-    }
-});
-let posts = newLocal;
-console.log(posts);
-
-
+    });
+    let posts = newLocal;
 
